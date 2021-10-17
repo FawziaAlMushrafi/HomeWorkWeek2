3aaa86f1d703fb29c5ce48f7d37498cd041d14ab
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-17 23:11:20 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    try {
+        var number = 5 / 5
+    }
+    catch (Example: ArithmeticException) {
+        if
+                (Example.message=="/5"){
+            println("NO Massage")
+        }
+
+    }}
